58611263883499a3fcac96979dc44eb8d4dac448
eve-atieno <atienoevelyne53@gmail.com>
2023-02-28 11:17:45 +0300

    added components

---
+++ src/components/CharitiesPage.js
@@ -0,0 +1,16 @@
+import Home from "../Home";
+import React, {useState,useEffect} from "react-router-dom";
+
+function CharitiesPage(){
+    const [charities,setCharities]= useState([]);
+
+useEffect(() => {
+        fetch("http://localhost:9292/test")
+        .then((response) => response.json())
+        // using async method to add events
+        .then((charitiesData) => {
+            console.log(charitiesData) 
+            return setCharities(() => charitiesData)})
+    }, [])
+}
+export default CharitiesPage
