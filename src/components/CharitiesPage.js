eba764b3a191e2d9b253247cf2022f8aa0c31410
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-02-28 12:25:23 +0300

    Set up cards to be held in EachCharity component.

--- src/components/CharitiesPage.js
+++ src/components/CharitiesPage.js
@@ -1,5 +1,6 @@
 import Home from "../Home";
 import React, {useState,useEffect} from "react-router-dom";
+import { Fragment } from "react";
 
 function CharitiesPage(){
     const [charities,setCharities]= useState([]);
@@ -12,5 +13,39 @@ useEffect(() => {
             console.log(charitiesData) 
             return setCharities(() => charitiesData)})
     }, [])
+
+    oneCharity = charities.map((charity) => {
+        return (
+            <EachCharity key={charity.id} charity={charity}/>
+        )
+    })
+
+    return (
+        <Fragment>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/allcharities" element={
+                    <Fragment>
+                        <div className="ui three column grid container" style={{
+                            
+                            display: 'flex',
+                            justifyContent: 'space-between', 
+                            alignItems: 'center' 
+                            }}>
+                            <div className="row">
+                                {oneCharity}
+                            </div>
+                            
+                        </div>
+                    </Fragment>
+                }>
+
+                </Route>
+
+
+            </Routes>
+
+        </Fragment>
+    )
 }
 export default CharitiesPage
---
+++ src/components/EachCharity.js
@@ -0,0 +1,52 @@
+import { Link } from 'react-router-dom';
+import { Card, Icon, Image, Button } from 'semantic-ui-react';
+
+function EachCharity({charity}) {
+    // destructuring the charity prop for easier access
+    const {id, name, image, description, location} = charity
+
+    return (
+        <Card className='card-style'>
+            {/* <Image src={image} wrapped ui={true} /> */}
+            <div className="rounded-top" style={{
+                height: 240,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundImage: `url(${image})`
+            }}
+            
+            ></div>
+            <Card.Content>
+            <Card.Header>{name}</Card.Header>
+            <Card.Meta>{location}</Card.Meta>
+            <Card.Description>
+                {description}
+            </Card.Description>
+            <br />
+            {/* <Card.Description>
+                {date} | {time}
+                <br />
+                <p className='money-bill'>
+                <Icon name="money bill alternate"/>
+                {price}
+                </p>
+            </Card.Description> */}
+            </Card.Content>
+            {/* <Card.Content extra>
+            <a>
+                
+                <Button primary style={{
+                  color: color,
+                  background: background
+                }}
+                onClick={updateAttend}
+                >
+                  <Icon name={willAttend ? 'paper plane outline' : 'time'} />{willAttend ? "RSVP'd" : "Attend"}
+                  </Button>
+                <Button secondary onClick={handleDelete}><Icon name='delete calendar' />Delete</Button>
+            </a>
+            </Card.Content> */}
+        </Card>
+    )
+}
