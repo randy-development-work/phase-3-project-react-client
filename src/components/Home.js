913c437d4104e75633c547fa0d641efd5beaec83
eve-atieno <atienoevelyne53@gmail.com>
2023-03-01 15:11:28 +0300

    edited Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,13 @@ function Home() {
              <h1 style={{ textAlign:'center',fontStyle: "italic", fontSize: "160px",fontWeight: "600"}}>Donate It</h1>
              <Nav.Link style={{ textDecoration: "none",display: "inline-block", color: "#fff", fontSize: "24px", border: "2px solid #fff", padding: "14px 70px", borderRadius: "50px", marginTop: ""}} href="/allcharities">"Don't use it? Donate It"</Nav.Link>
         </div>
-        <div>
+        <div backgroundSize="cover" backgroundPosition="centre" >
             {/* <video autoPlay loop muted playsInline className="backVideo" style={{ position: 'fixed', right: '0', bottom: '0', zIndex: '-1' }}>
                 <source src="./videoplayback.mp4" type="video/mp4" style={{ width: '100%', height: '100%' }} />
             </video> */}
-            <img className ="backVideo" style={{ width: '100%', height: '100%' }} src = "https://images.unsplash.com/photo-1603827457577-609e6f42a45e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGRvbmF0aW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"></img>
-            <h1>Name</h1>
+            <img className ="backVideo"   bac style={{ width: '100%', height: '100%' }} src = "https://images.unsplash.com/photo-1603827457577-609e6f42a45e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGRvbmF0aW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60">
+              
+            </img>
         </div>
         <style jsx>
          {`
